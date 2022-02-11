b5af5fef0388ed33eeb9865f19aac6c8fb0084b7
Dmitry <solonkou4eba@mai.ru>
2022-02-11 21:01:05 +0300

    All in ViewModel

--- HomeWork2/LaptopViewModel.cs
+++ HomeWork2/LaptopViewModel.cs
@@ -12,14 +12,31 @@ using System.Windows.Input;
 
 namespace HomeWork2
 {
+    public class Root
+    {
+        public int postId { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string body { get; set; }
+    }
 
     public class LaptopViewModel : INotifyPropertyChanged
     {
+        private const string url = "https://jsonplaceholder.typicode.com/comments";
+        private HttpClient _Client = new HttpClient();
+        private ObservableCollection<Root> _post;
         public event PropertyChangedEventHandler PropertyChanged;
         private Laptop laptop;
         private int _someInt = 123;
 
-
+        public async Task Output()
+        {
+            var content = await _Client.GetStringAsync(url);
+            var root = JsonConvert.DeserializeObject<List<Root>>(content);
+            _post = new ObservableCollection<Root>(root);
+            
+        }
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
@@ -45,6 +62,10 @@ namespace HomeWork2
             await ChangeInt();
         });
 
+        public LaptopViewModel()
+        {
+            laptop = new Laptop();
+        }
 
 
 
--- HomeWork2/MainPage.xaml.cs
+++ HomeWork2/MainPage.xaml.cs
@@ -12,19 +12,9 @@ using Newtonsoft.Json;
 namespace HomeWork2
 {
     // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
-    public class Root
-    {
-        public int postId { get; set; }
-        public int id { get; set; }
-        public string name { get; set; }
-        public string email { get; set; }
-        public string body { get; set; }
-    }
+    
     public partial class MainPage : ContentPage
     {
-        private const string url = "https://jsonplaceholder.typicode.com/comments";
-        private HttpClient _Client = new HttpClient();
-        private ObservableCollection<Root> _post;
         private LaptopViewModel vm = new LaptopViewModel();
         public MainPage()
         {
@@ -33,10 +23,7 @@ namespace HomeWork2
         }
         protected override async void OnAppearing()
         {
-            var content = await _Client.GetStringAsync(url);
-            var root = JsonConvert.DeserializeObject<List<Root>>(content);
-            _post = new ObservableCollection<Root>(root);
-            Post_List.ItemsSource = _post;
+
             base.OnAppearing();
 
             await vm.ChangeInt();
